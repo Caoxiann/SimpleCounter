b5645a1572e347227622f4ae06830d5816ad24f7
陈浩贤 <826919861@qq.com>
2017-03-14 23:29:06 +0800

    修复不同屏幕间的适配问题

--- SimpleCounter/SimpleCounter/ChartViewController.m
+++ SimpleCounter/SimpleCounter/ChartViewController.m
@@ -26,14 +26,19 @@
     [super viewDidLoad];
     self.lineView.layer.cornerRadius=10;
     
-    NSArray *dayArray=[NSArray arrayWithObjects:@"1",@"2",@"1",@"2",@"1",@"2",@"1", nil];
-    self.dataArray=[NSArray arrayWithObjects:@(120.11),@(0),@(211),@(32),@(25),@(16),@(22), nil];
-    
+
+}
+
+-(void)viewDidLayoutSubviews{
+    [super viewDidLayoutSubviews];
     lineViewHeight=self.lineView.bounds.size.height;
     lineViewWidth=self.lineView.bounds.size.width;
     graphViewWidth=self.graphView.bounds.size.width;
     graphViewHeight=self.graphView.bounds.size.height;
 
+    NSArray *dayArray=[NSArray arrayWithObjects:@"1",@"2",@"1",@"2",@"1",@"2",@"1", nil];
+    self.dataArray=[NSArray arrayWithObjects:@(120.11),@(0),@(211),@(32),@(25),@(16),@(22), nil];
+    
     self.result=[self calculatePricePerPixcialAndAverageWithDataArray:self.dataArray];
     
     
@@ -44,8 +49,6 @@
     [self drawLinesWithResult:self.result data:self.dataArray];
     [self creatLabelOnXwithData:dayArray];
 }
-
-
 - (IBAction)segementDidChange:(id)sender {
     [self performSegueWithIdentifier:@"showTableVC" sender:self];
 }
@@ -59,8 +62,8 @@
 -(void)drawAxis{
     UIBezierPath *axis=[UIBezierPath bezierPath];
     [axis moveToPoint:CGPointMake(10, 20)];
-    [axis addLineToPoint:CGPointMake(10, lineViewHeight)];
-    [axis addLineToPoint:CGPointMake(lineViewWidth+20, lineViewHeight)];
+    [axis addLineToPoint:CGPointMake(10, lineViewHeight-20)];
+    [axis addLineToPoint:CGPointMake(lineViewWidth, lineViewHeight-20)];
     axis.lineWidth=1.0f;
     axis.lineJoinStyle=kCGLineJoinRound;
     
@@ -74,10 +77,10 @@
 
 -(void)creatLabelOnXwithData:(NSArray *)dataArray{
     CGFloat days=[dataArray count];
-    CGFloat spacing=lineViewWidth/(days-1);
+    CGFloat spacing=lineViewWidth/days;
     
     for (int i=0; i<days; i++) {
-        UILabel *dayLabel=[[UILabel alloc]initWithFrame:CGRectMake(10+spacing*i, lineViewHeight+5, spacing,15)];
+        UILabel *dayLabel=[[UILabel alloc]initWithFrame:CGRectMake(10+spacing*i, lineViewHeight-15, spacing,15)];
         dayLabel.text=[dataArray objectAtIndex:i];
         dayLabel.textAlignment=NSTextAlignmentLeft;
         [dayLabel setFont:[UIFont systemFontOfSize:12 weight:0.1]];
@@ -106,7 +109,7 @@
 
 -(void)drawFadeBackgroundView{
     CAGradientLayer *gradientLayer=[CAGradientLayer layer];
-    gradientLayer.frame=CGRectMake(0, 0, lineViewWidth+30, lineViewHeight+30);
+    gradientLayer.frame=self.lineView.bounds;
     gradientLayer.startPoint=CGPointMake(0, 0.0);
     gradientLayer.endPoint=CGPointMake(1, 0.0);
     
@@ -121,8 +124,8 @@
     CGFloat arr[]={10,1};
     
     UIBezierPath *dashLine=[UIBezierPath bezierPath];
-    [dashLine moveToPoint:CGPointMake(10, lineViewHeight-result.x*result.y)];
-    [dashLine addLineToPoint:CGPointMake(lineViewWidth,lineViewHeight-result.x*result.y)];
+    [dashLine moveToPoint:CGPointMake(10, lineViewHeight-result.x*result.y-20)];
+    [dashLine addLineToPoint:CGPointMake(lineViewWidth,lineViewHeight-result.x*result.y-20)];
     [dashLine setLineDash:arr count:2 phase:0];
     
     
@@ -136,18 +139,21 @@
 }
 
 -(void)drawPointsWithResult:(CGPoint)result data:(NSArray *)dataArray{
-    CGFloat spacing=lineViewWidth/([dataArray count]-1);
+    CGFloat spacing=lineViewWidth/[dataArray count];
     CGFloat ppp=result.x;
     UIBezierPath *points=[UIBezierPath bezierPath];
     CAShapeLayer *pointsLayer=[CAShapeLayer layer];
     for (int i=0; i<[dataArray count]; i++) {
         float price=[[dataArray objectAtIndex:i]floatValue];
-        [points appendPath:[UIBezierPath bezierPathWithArcCenter:CGPointMake(10+spacing*i, lineViewHeight-ppp*price) radius:4 startAngle:0 endAngle:2*M_PI clockwise:0]];
+        [points appendPath:[UIBezierPath bezierPathWithArcCenter:CGPointMake(10+spacing*i, lineViewHeight-ppp*price-20) radius:4 startAngle:0 endAngle:2*M_PI clockwise:0]];
     }
     pointsLayer.path=points.CGPath;
     pointsLayer.fillColor=[UIColor whiteColor].CGColor;
     pointsLayer.opacity=0.8;
-    pointsLayer.frame=self.lineView.bounds;    
+    pointsLayer.frame=self.lineView.bounds;
+    
+    NSLog(@"layer:%@,lineview:%@",NSStringFromCGRect (pointsLayer.frame),NSStringFromCGRect( _lineView.bounds));
+    
     CASpringAnimation *springAni=[CASpringAnimation animationWithKeyPath:@"position.y"];
     springAni.mass=1;
     springAni.stiffness=100;
@@ -166,12 +172,12 @@
 
 -(void)drawLinesWithResult:(CGPoint)result data:(NSArray *)dataArray{
     UIBezierPath *strokePath=[UIBezierPath bezierPath];
-    CGFloat spacing=lineViewWidth/([dataArray count]-1);
+    CGFloat spacing=lineViewWidth/[dataArray count];
     CGFloat ppp=result.x;
-    [strokePath moveToPoint:CGPointMake(10, lineViewHeight-ppp*[[dataArray objectAtIndex:0]floatValue])];
+    [strokePath moveToPoint:CGPointMake(10, lineViewHeight-ppp*[[dataArray objectAtIndex:0]floatValue]-20)];
     for (int i=1; i<[dataArray count]; i++) {
         float price=[[dataArray objectAtIndex:i]floatValue];
-        [strokePath addLineToPoint:CGPointMake(10+i*spacing, lineViewHeight-ppp*price)];
+        [strokePath addLineToPoint:CGPointMake(10+i*spacing, lineViewHeight-ppp*price-20)];
         NSLog(@"line height:%f",strokePath.currentPoint.y);
     }
     CAShapeLayer *layer2=[CAShapeLayer layer];
@@ -214,20 +220,18 @@
     CGPoint tapPoint=[sender locationInView:self.lineView];
     CGPoint hitData=[self getDataIfHitPoint:tapPoint withData:self.dataArray AndResult:self.result];
     if (hitData.x!=-1&&hitData.y!=-1) {
-        NSLog(@"hit %f",hitData.y);
         [self showHitPrice:hitData];
     }
 }
 
 -(CGPoint)getDataIfHitPoint:(CGPoint)tapPoint withData:(NSArray *)dataArray AndResult:(CGPoint)result{
-    CGFloat positionX,positionY,price,ppp=result.x;
-    CGFloat spacing=lineViewWidth/([dataArray count]-1);
+    CGFloat positionX,price;
+    CGFloat spacing=lineViewWidth/[dataArray count];
     for (int i=0; i<[dataArray count]; i++) {
         price=[[dataArray objectAtIndex:i]floatValue];
         positionX=i*spacing+10;
-        positionY=lineViewHeight-ppp*price;
         
-        if (tapPoint.x>positionX-10&&tapPoint.x<positionX+10&&tapPoint.y>positionY-10&&tapPoint.y<positionY+10) {
+        if (tapPoint.x>positionX-10&&tapPoint.x<positionX+10) {
             return CGPointMake(positionX, price);
         }
     }
